07f09056fdd36239e65504725ae78736a627bc1b
emeraudeLa <esmeralda.martin206@gmail.com>
2025-01-29 11:57:56 +0100

    Fini Table.java

--- TD/TD2/Table.java
+++ TD/TD2/Table.java
@@ -11,11 +11,42 @@ public class Table{
 
     public void affTable(){
         for (int i=0;i<tableDom.size();i++){
-            tableDom[i].afficher()
+            tableDom.get(i).afficher();
         }
+    }
+    public int recherche(int g, int d){
+        int resultat = 0;
+        if(tableDom.get(0).getGauche()==d){
+            resultat=-1;
+        }
+        else if (tableDom.get(tableDom.size()-1).getDroite()==g){//prend la valeur a droite du domino le plus a droite (size -1) et compare avec le cote gauche du dom
+            resultat=1;
+        }
+        return resultat;
+    }
+
+    public void poserAgauche(Domino dom){
+        tableDom.add(0,dom);
+    }
+
+    public void poserAdroite(Domino dom){
+        tableDom.add(dom);
     }
 
     public static void main(String[] args){
-        System.out.println("test");
+        Domino d1 = new Domino(2, 4);
+        Domino d2 = new Domino(4, 5);
+        Domino d3 = new Domino(5, 3);
+        Domino d4 = new Domino(3, 6);
+        Domino d5 = new Domino(6, 1);
+        Domino d6 = new Domino(1, 2);
+        Table t = new Table(6);
+        t.initTable(d1);
+        t.poserAdroite(d2);
+        t.poserAdroite(d3);
+        t.poserAdroite(d4);
+        t.poserAdroite(d5);
+        t.poserAdroite(d6);
+        t.affTable();
     }
 }
