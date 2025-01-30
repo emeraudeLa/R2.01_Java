5cc7d58343069bb3edacd0cbc89a5bef8c76e00c
emeraudeLa <esmeralda.martin206@gmail.com>
2025-01-30 09:22:05 +0100

    Update Voiture.java

--- TP/TP2/Partie1/Voiture.java
+++ TP/TP2/Partie1/Voiture.java
@@ -4,7 +4,53 @@ public class Voiture {
     private float vitesse;
     private float kilometre;
 
-    public Voiture(){
-        this.identifiant=this.identifiant+1;
+    public static int nbVoitures=0;
+
+    //Builder
+    public Voiture(String marque, float vitesse, float kilometre){
+        nbVoitures=nbVoitures+1;
+        this.identifiant=nbVoitures;
+        /*
+        this.setMarque(marque);
+        this.setVitesse(vitesse);
+        this.setKilometre(kilometre);
+        */
+        this.marque=marque;
+        this.vitesse=vitesse;
+        this.kilometre=kilometre;
     }
+
+    //Setters
+    public void setMarque(String marque){
+        this.marque=marque;
+    }
+    public void setVitesse(float vitesse){
+        this.vitesse=vitesse;
+    }
+    public void setKilometre(float kilometre){
+        this.kilometre=kilometre;
+    }
+
+    //Getters
+    public int getId(){return identifiant;}
+    public String getMarque(){return marque;}
+    public float getVitesse(){return vitesse;}
+    public float getKilometre(){return kilometre;}
+
+    public String toString(){
+        return "("+identifiant+" | "+marque+" | "+vitesse+" | "+kilometre+")";
+    }
+
+    public void afficher(){
+        System.out.println(toString());
+    }
+    
+    public static void main(String[] args){
+        Voiture v1 = new Voiture("citroen",150.7f,546.8f);
+        v1.afficher();
+        Voiture v2 = new Voiture("peugeot",1000.7f,754.8f);
+        
+        v2.afficher();
+    }
+    
 }
