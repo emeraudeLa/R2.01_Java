dfed29f531bd82512ee78e49685f75a69efa9f40
emeraudeLa <esmeralda.martin206@gmail.com>
2025-01-30 08:43:58 +0100

    Add files via upload

---
+++ TP/TP2/Partie1/Voiture.java
@@ -0,0 +1,10 @@
+public class Voiture {
+    private int identifiant;
+    private String marque;
+    private float vitesse;
+    private float kilometre;
+
+    public Voiture(){
+        this.identifiant=this.identifiant+1;
+    }
+}
