301d1e4da25166cd0ea17c087b21a6576ff0c8c7
Esmeralda <esmeralda.martin206@gmail.com>
2025-02-06 09:23:09 +0100

    Update Date.java

--- TP/TP2/Partie2/Date.java
+++ TP/TP2/Partie2/Date.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Date {
     private int jour;
     private int mois;
@@ -7,6 +9,34 @@ public class Date {
         this.jour = jour;
         this.mois = mois;
         this.annee = annee;
+    }
+
+    public void entreeDate(Scanner entree){
+        String d;
+        String j;
+        String a;
+        System.out.println("Jour :");
+        d=entree.next();
+        jour=Integer.parseInt(d);
+        System.out.println("Mois :");
+        j=entree.next();
+        mois=Integer.parseInt(j);
+        System.out.println("Annee :");
+        a=entree.next();
+        annee=Integer.parseInt(a);
+    }
+
+    public void afficher(){
+        System.out.println(jour+"/"+mois+"/"+annee);
+    }
+
+    public static void main(String[] args){
+        Scanner entree = new Scanner(System.in);//ouverture scanner
+        Date today = new Date(7,8,2006);
+        today.afficher();
+        today.entreeDate(entree);
+        today.afficher();
+        entree.close();//ferme le scanner
     }
 
 }
